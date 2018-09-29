a8cff7de6b59ada261fce79bcaedb3472854b5c6
vc049147 <varsha.chitturi@cerner.com>
2018-09-28 19:39:01 -0500

    View of all pull requests of selected project

---
+++ app/src/main/java/com/varshach/myapplication/SplitDiffActivity.java
@@ -0,0 +1,13 @@
+package com.varshach.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplitDiffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_diff);
+    }
+}
